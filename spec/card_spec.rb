2cbf12aa0e4f0effe4c71295d5f7a8385b5a557b
Riyaad <riyaadhowlader@gmail.com>
2024-03-20 17:23:53 -0500

    Card Tests

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -5,5 +5,18 @@ Card Tests
 require 'card'
 
 Rspec.describe Card do
+  let(:new_card) { Card.new('Hearts', '2') }
 
+  describe '#initialize' do
+    it 'creates a card' do
+      expect(new_card.suit).to eq('Hearts')
+      expect(new_card.value).to eq('2')
+    end
+  end
+
+  describe '.cardname' do
+    context 'reads out the card' do
+      it { expect(new_card.cardname).to eq('2 of Hearts') }
+    end
+  end
 end
