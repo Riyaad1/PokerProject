f43b44ba0d515ef416f64e3f531d413488ad504c
Riyaad <riyaadhowlader@gmail.com>
2024-03-20 17:57:42 -0500

    .deal tests

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe Deck do
       it { expect(new_deck.deck.length).to_not eq(1) }
     end
   end
+
+  describe '.deal' do
+    it 'deals cards first round' do
+      new_deck.first_turn = true
+      expect(new_deck.deal.length).to eq(5)
+    end
+    context 'deals cards other rounds' do
+      it { expect(new_deck.deal.length).to eq(1) }
+    end
+  end
 end
