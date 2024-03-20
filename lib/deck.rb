68030c1bba99772c4afea989aef790d87cad8c49
Riyaad <riyaadhowlader@gmail.com>
2024-03-20 17:41:17 -0500

    initialize complete

--- lib/deck.rb
+++ lib/deck.rb
@@ -2,6 +2,20 @@
 The deck containing cards
 '''
 
-class Deck
+require 'card'
 
+class Deck
+  attr_reader :deck, :first_turn
+  attr_writer :first_turn
+
+  def initialize
+    @suits = ['Clubs', 'Spades', 'Hearts', 'Diamonds']
+    @values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+    @deck = []
+    @first_turn = false
+
+    @suits.product(@values) do |x, y|
+      @deck << Card.new(x, y)
+    end
+  end
 end
