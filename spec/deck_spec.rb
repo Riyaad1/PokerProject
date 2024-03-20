7b71d7c69eae0c781af443dec16e4dd096d373e8
Riyaad <riyaadhowlader@gmail.com>
2024-03-20 17:34:22 -0500

    First deck test

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -5,5 +5,11 @@ Deck Tests
 require 'deck'
 
 RSpec.describe Deck do
+  let(:new_deck) { Deck.new }
 
+  describe '#initialize' do
+    context 'sets up deck' do
+      it { expect(new_deck.deck.length).to_not eq(1) }
+    end
+  end
 end
