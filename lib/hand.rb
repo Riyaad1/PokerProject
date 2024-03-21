b85d723325aed7a18081290c2e2eff42f32590cf
Riyaad <riyaadhowlader@gmail.com>
2024-03-21 10:03:51 -0500

    First part of determine method, since code for determine will become long if putting all hand types in, will split it into seperate methods to call

--- lib/hand.rb
+++ lib/hand.rb
@@ -3,5 +3,29 @@ Holds the player deck and winning hands
 '''
 
 class Hand
+  attr_reader :current_hand, :high_card, :player_deck
+  attr_writer :current_hand, :high_card, :player_deck
 
+  RANKING = ['Royal Flush', 'Straight Flush', 'Four-of-a-Kind', 'Full House', 'Flush', 'Straight', 'Three-of-a-Kind', 'Two Pair', 'One Pair', 'High Card']
+
+  def initialize(player_deck)
+    @current_hand = ''
+    @high_card = '0'
+    @player_deck = player_deck
+  end
+
+  def determine
+    suits = []
+    values = []
+    faces = {'Jack' => '11', 'Queen' => '12', 'King' => '13', 'Ace' => '14'}
+    @player_deck.each do |x|
+      suits << x.suit
+      if faces.key?(x.value)
+        values << faces[x.value]
+      else
+        values << x.value
+      end
+    end
+    values.sort!
+  end
 end
