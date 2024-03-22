101dc3245165701083a03556d0c24c439ca205b8
Riyaad <riyaadhowlader@gmail.com>
2024-03-22 15:13:11 -0500

    initialize done

--- lib/player.rb
+++ lib/player.rb
@@ -4,5 +4,13 @@ Defines the players
 require 'hand'
 
 class Player
+  attr_reader :folded, :player_pot, :hand
+  attr_writer :player_pot
 
+  def initialize(player_deck, player_pot)
+    @hand = Hand.new(player_deck)
+    @player_pot = player_pot.to_i
+    @player_bet = 0
+    @folded = false
+  end
 end
